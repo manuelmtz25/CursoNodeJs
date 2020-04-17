a54c8c1573d06735d9ccd633f460a81386e088d1
Manuel Martínez <manuelmtz_25@hotmail.com>
2020-04-17 17:57:11 -0500

    .

--- server.js
+++ server.js
@@ -8,21 +8,21 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(router);
 
 router.get("/message", function (req, res) {
-  console.log(req.headers);
-  res.header({
-      "custom-header": "Nuestro valor personalizado"
-      
-  })
-  res.send("Lista de mensajes");
+    console.log(req.headers);
+    res.header({
+        "custom-header": "Nuestro valor personalizado"
+        
+    })
+    res.send("Lista de mensajes");
 });
 
 router.post("/message", function (req, res) {
-  console.log(req.query);
-  console.log(req.body);
-  //res.send(`Mensaje -${req.body.text}- añadido correctamente`);
-  //res.send() //respuesta vacía
-  //res.status(201).send() //respuesta con estado
-  res.status(201).send({error:'', body:'Creado correctamente'}) //respuesta estructurada
+    console.log(req.query);
+    console.log(req.body);
+    //res.send(`Mensaje -${req.body.text}- añadido correctamente`);
+    //res.send() //respuesta vacía
+    //res.status(201).send() //respuesta con estado
+    res.status(201).send({error:'', body:'Creado correctamente'}) //respuesta estructurada
 });
 
 // router.get('/', function(req, res){
@@ -34,7 +34,7 @@ router.post("/message", function (req, res) {
 // })
 
 // app.use("/", function (req, res) {
-//   res.send("<h1>Hola</h1>");
+//     res.send("<h1>Hola</h1>");
 // });
 
 app.listen(3000);
