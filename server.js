8c17827b3803ef0325532a7f8cbc35aee9c8f01a
Manuel Martínez <manuelmtz_25@hotmail.com>
2020-04-17 17:40:51 -0500

    Recibir información desde el cliente: Body y Query

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = express.Router();
 
 var app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(router);
 
-router.get('/message', function(req, res){
-    res.send('Lista de mensajes');
-})
+router.get("/message", function (req, res) {
+  res.send("Lista de mensajes");
+});
 
-router.post('/message', function(req, res){
-    res.send('Mensaje añadido');
-})
+router.post("/message", function (req, res) {
+  console.log(req.query);
+  console.log(req.body);
+  res.send(`Mensaje -${req.body.text}- añadido correctamente`);
+});
 
 // router.get('/', function(req, res){
 //     res.send('Hola desde GET');
