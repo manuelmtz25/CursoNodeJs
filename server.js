1104027024c63129d69ed8b4e0af3399c506ed4b
Manuel Martínez <manuelmtz_25@hotmail.com>
2020-04-17 17:16:55 -0500

    express.Router GET y POST

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const router = express.Router();
 
 var app = express();
 
-app.use('/', function(req, res){
-    res.send('<h1>Hola</h1>');
-});
+app.use(router);
+
+router.get('/message', function(req, res){
+    res.send('Lista de mensajes');
+})
+
+router.post('/message', function(req, res){
+    res.send('Mensaje añadido');
+})
+
+// router.get('/', function(req, res){
+//     res.send('Hola desde GET');
+// })
+
+// router.post('/', function(req, res){
+//     res.send('Hola desde POST');
+// })
+
+// app.use("/", function (req, res) {
+//   res.send("<h1>Hola</h1>");
+// });
 
 app.listen(3000);
-console.log('La aplicación está escuchando en http://localhost:3000');
+console.log("La aplicación está escuchando en http://localhost:3000");
