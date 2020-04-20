50f76a4e990ab122fc580c315dc6f99a52e53799
Manuel Martínez <manuelmtz_25@hotmail.com>
2020-04-20 18:10:34 -0500

    Habilitar CORS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const socket = require('./socket');
 const db = require('./db');
@@ -10,6 +11,8 @@ const router = require('./network/routes');
 db(
     'mongodb+srv://platzi-admin:vmorelos@curso-platzi-de4ii.mongodb.net/telegrom?retryWrites=true&w=majority'
 );
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
