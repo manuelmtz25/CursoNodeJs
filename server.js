3a558db4e4db3ca1abcc37e03cb68e62ab208833
Manuel Martínez <manuelmtz_25@hotmail.com>
2020-04-20 17:59:58 -0500

    Conectar la API al servidor de WebSockets

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const app = express();
+const server = require('http').Server(app);
 
+const bodyParser = require('body-parser');
+const socket = require('./socket');
 const db = require('./db');
 const router = require('./network/routes');
 
@@ -8,12 +11,15 @@ db(
     'mongodb+srv://platzi-admin:vmorelos@curso-platzi-de4ii.mongodb.net/telegrom?retryWrites=true&w=majority'
 );
 
-var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+socket.connect(server);
+
 router(app);
 
 app.use('/app', express.static('public'));
 
-app.listen(3000);
-console.log('La aplicación está escuchando en http://localhost:3000');
+server.listen(3000, function () {
+    console.log('La aplicación está escuchando en http://localhost:3000');
+});
